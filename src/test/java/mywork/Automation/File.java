fee932fb0ac27b7bee798e71086bcf8b9bd41374
sujata <sujata.kartikey@gmail.com>
2020-04-19 10:55:55 +0530

    First commit

---
+++ src/test/java/mywork/Automation/File.java
@@ -0,0 +1,23 @@
+package mywork.Automation;
+
+import org.testng.annotations.Test;
+
+public class File {
+	@Test
+	public void first(){
+			
+			System.out.println("first tc");
+	}
+			public void Second(){
+				
+				System.out.println("second tc");
+			}
+				public void Thirdt(){
+					
+					System.out.println("third tc");
+					
+				
+			//driver.switchTo().frame("Hi");
+			
+	}
+}
